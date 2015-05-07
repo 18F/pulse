8dd200ca5cdbe4e324dfb26ed59c2e088e647511
Eric Mill <eric@konklone.com>
2015-05-06 20:01:04 -0400

    vendor bourbon and neat, change url structure, de-minify

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,13 @@
 var gulp        = require('gulp'),
     sass        = require('gulp-sass'),
     watch       = require('gulp-watch'),
-    minifycss   = require('gulp-minify-css'),
     rename      = require('gulp-rename'),
     livereload  = require('gulp-livereload');
 
 gulp.task('sass', function() {
     return gulp.src('assets/scss/main.scss')
         .pipe(sass())
-        .pipe(minifycss())
-        .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('css'))
+        .pipe(gulp.dest('assets/css'))
         .pipe(livereload());
 });
 
@@ -26,7 +23,5 @@ gulp.task('default', ['sass', 'watch']);
 gulp.task('travis', function() {
     return gulp.src('assets/scss/main.scss')
     .pipe(sass())
-    .pipe(minifycss())
-    .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('assets/css'));
 });
