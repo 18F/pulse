c888899e58a08246f365805b7de17e1dea02ea71
Eric Mill <eric@konklone.com>
2015-05-19 00:32:15 -0400

    update DAP filtering, renamed row

--- assets/js/analytics.js
+++ assets/js/analytics.js
@@ -1,46 +1,8 @@
 $(document).ready(function () {
 
   $.get("/assets/data/tables/analytics-domains.json", function(data) {
-    var prepared = prepareData(data.data);
-    renderTable(prepared);
+    renderTable(data.data);
   });
-
-  /**
-
-    Incoming and outgoing rows are the same:
-
-    {
-      "Branch": "executive",
-      "Domain": "acus.gov",
-      "Live": "True",
-      "Participates in Analytics": "False",
-      "Redirect": "False"
-    }
-  */
-
-  // TODO: This data should come from the JSON, not the CSV -> JSON.
-  var prepareData = function(domains) {
-    var prepared = [];
-    for (var i=0; i<domains.length; i++) {
-      var domain = domains[i];
-
-      // TODO: Move this to the data preparation stage.
-      // Filter step:
-      if (domain["Live"] == "False")
-        continue;
-
-      if (domain["Redirect"] == "True")
-        continue;
-
-      if (domain["Branch"] != "executive")
-        continue;
-
-      prepared.push(domain);
-    }
-
-    return prepared;
-  }
-
 
   var renderTable = function(data) {
     $("table").DataTable({
@@ -48,10 +10,8 @@ $(document).ready(function () {
 
       columns: [
         {"data":"Domain"},
-        {"data":"Participates in Analytics"}
-      ],
-
-      dom: "lrtip"
+        {"data":"Participates in DAP"}
+      ]
     });
   };
 
