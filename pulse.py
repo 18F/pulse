932ec194081b8c46e7d8d6b25c287738fcce6852
Eric Mill <eric.mill@gsa.gov>
2015-09-10 13:14:39 -0400

    properly embed site metadata

--- pulse.py
+++ pulse.py
@@ -2,18 +2,29 @@
 
 from flask import Flask
 from flask import render_template
+import yaml
+import datetime
+
 
 # Initialize app.
 app = Flask(__name__)
 app.debug = True
 
-# Routes.
+# Make site metadata available everywhere.
+meta = yaml.safe_load(open("meta.yml"))
+@app.context_processor
+def inject_meta():
+	return dict(site=meta, now=datetime.datetime.utcnow)
 
+# Routes.
 @app.route("/")
 def index():
 	return render_template("index.html")
 
-
+# Filters.
+@app.template_filter('date')
+def datetimeformat(value, format='%H:%M / %d-%m-%Y'):
+    return value.strftime(format)
 
 # Boot it up.
 
