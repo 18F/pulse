4e3d2156210537b1f2220bfae836333f6e8f416a
Eric Mill <eric.mill@gsa.gov>
2015-09-10 12:38:35 -0400

    make base template and home page work in Flask and jinja2

--- pulse.py
+++ pulse.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python
 
 from flask import Flask
-app = Flask(__name__)
+from flask import render_template
 
+# Initialize app.
+app = Flask(__name__)
+app.debug = True
+
+# Routes.
 
 @app.route("/")
-def main():
-	return "Hello, world!"
+def index():
+	return render_template("index.html")
+
+
+
+# Boot it up.
 
 if __name__ == "__main__":
     app.run()
