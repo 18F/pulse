bf79bb4b079bf35aa60c9a72831c15b40619b611
Susan Tan <susan.tan.fleckerl@gmail.com>
2015-12-23 21:01:45 -0800

    Convert tabs to spaces in pulse.py to follow pep8

--- pulse.py
+++ pulse.py
@@ -28,7 +28,7 @@ from app import data
 meta = yaml.safe_load(open("meta.yml"))
 @app.context_processor
 def inject_meta():
-	return dict(site=meta, now=datetime.datetime.utcnow)
+    return dict(site=meta, now=datetime.datetime.utcnow)
 
 @app.template_filter('date')
 def datetimeformat(value, format='%H:%M / %d-%m-%Y'):
@@ -36,61 +36,61 @@ def datetimeformat(value, format='%H:%M / %d-%m-%Y'):
 
 @app.template_filter('field_map')
 def field_map(value, category=None, field=None):
-	return data.data[category][field][str(value)]
+    return data.data[category][field][str(value)]
 
 @app.template_filter('analytics_map')
 def analytics_map(value, field=None):
-	return data.data['analytics'][field][str(value)]
+    return data.data['analytics'][field][str(value)]
 
 ###
 # Routes
 @app.route("/")
 def index():
-	return render_template("index.html")
+    return render_template("index.html")
 
 @app.route("/about/")
 def about():
-	return render_template("about.html")
+    return render_template("about.html")
 
 @app.route("/https/domains/")
 def https_domains():
-	return render_template("https/domains.html")
+    return render_template("https/domains.html")
 
 @app.route("/https/agencies/")
 def https_agencies():
-	return render_template("https/agencies.html")
+    return render_template("https/agencies.html")
 
 @app.route("/https/guidance/")
 def https_guide():
-	return render_template("https/guide.html")
+    return render_template("https/guide.html")
 
 @app.route("/analytics/domains/")
 def analytics_domains():
-	return render_template("analytics/domains.html")
+    return render_template("analytics/domains.html")
 
 @app.route("/analytics/agencies/")
 def analytics_agencies():
-	return render_template("analytics/agencies.html")
+    return render_template("analytics/agencies.html")
 
 @app.route("/analytics/guidance/")
 def analytics_guide():
-	return render_template("analytics/guide.html")
+    return render_template("analytics/guide.html")
 
 # TODO: Take in an agency slug, look up data and pass it to template.
 @app.route("/agency/<slug>")
 def agency(slug=None):
-	if agencies.get(slug) is None:
-		pass # TODO: 404
+    if agencies.get(slug) is None:
+        pass # TODO: 404
 
-	return render_template("agency.html", agency=agencies[slug])
+    return render_template("agency.html", agency=agencies[slug])
 
 # TODO: Take in a domain name, look up data and pass it to template.
 @app.route("/domain/<hostname>")
 def domain(hostname=None):
-	if domains.get(hostname) is None:
-		pass # TODO: 404
+    if domains.get(hostname) is None:
+        pass # TODO: 404
 
-	return render_template("domain.html", domain=domains[hostname])
+    return render_template("domain.html", domain=domains[hostname])
 
 
 
