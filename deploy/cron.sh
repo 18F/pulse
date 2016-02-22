bcc3295e5bb90e0ec93805a1ac00b890d41de392
Eric Mill <eric.mill@gsa.gov>
2016-02-21 22:52:10 -0500

    use bashrc, not home dir virtualenvs

--- deploy/cron.sh
+++ deploy/cron.sh
@@ -3,11 +3,14 @@
 # Set the path to domain-scan.
 export DOMAIN_SCAN_PATH=/opt/scan/domain-scan/scan
 
+
 # go to pulse environment home
 cd $HOME/pulse/$PULSE_ENV/current
 
-# load virtualenv
-source $HOME/.virtualenvs/pulse-$PULSE_ENV/bin/activate
+# load environment and virtualenv
+source $HOME/.bashrc
+workon pulse-$PULSE_ENV
+# source $HOME/.virtualenvs/pulse-$PULSE_ENV/bin/activate
 
 # run the relevant env-specific data update path
 make update_$PULSE_ENV
