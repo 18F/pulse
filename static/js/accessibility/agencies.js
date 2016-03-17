14687c7c4b15b8272624173d817eb7dba6ea7be7
Nick Bristow <Nick.Bristow@gsa.gov>
2016-03-17 16:58:47 -0400

    updated agency page, still uses https data

--- static/js/accessibility/agencies.js
+++ static/js/accessibility/agencies.js
@@ -1,8 +1,18 @@
 $(document).ready(function () {
 
-  $.get("/static/data/tables/https/agencies.json", function(data) {
+  $.get("/data/agencies/https.json", function(data) {
     renderTable(data.data);
   });
+
+  var percentBar = function(field) {
+    return function(data, type, row) {
+      if (type == "sort")
+        return null;
+      return Utils.progressBar(Utils.percent(
+        row.https[field], row.https.eligible
+      ));
+    };
+  }
 
   var renderTable = function(data) {
     $("table").DataTable({
@@ -12,15 +22,16 @@ $(document).ready(function () {
       data: data,
 
       columns: [
-        {data: "Agency"},
+        {data: "name"},
         {
-          data: "Number of Domains",
-          render: Utils.filterAgency("https")
+          data: "https.eligible",
+          render: Utils.filterAgency("accessibility")
         },
-        {data: "Uses HTTPS"},
-        {data: "Enforces HTTPS"},
-        {data: "Strict Transport Security (HSTS)"},
-        {data: "SSL Labs (A- or higher)"}
+        {data: "https.uses"},
+        {data: "https.enforces"},
+        {data: "https.hsts"},
+        {data: "https.grade"},
+        {data: "https.uses"}
       ],
 
       // order by number of domains
@@ -35,8 +46,24 @@ $(document).ready(function () {
           }
         },
         {
-          render: Utils.progressBar, 
-          targets: [2,3,4,5],
+          render: percentBar("uses"),
+          targets: 2,
+        },
+        {
+          render: percentBar("enforces"),
+          targets: 3,
+        },
+        {
+          render: percentBar("hsts"),
+          targets: 4,
+        },
+        {
+          render: percentBar("grade"),
+          targets: 5,
+        },
+        {
+          render: percentBar("uses"),
+          targets: 6,
         }
       ],
 
