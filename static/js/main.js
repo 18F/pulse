b7f47cf63e6c5ca78a1d3cd707487e15b5508694
xtine <christine@fieryorange.com>
2016-05-18 17:54:47 -0700

    responsive width dataTable

--- static/js/main.js
+++ static/js/main.js
@@ -17,6 +17,9 @@ $(function(){
   });
 
   $('table').on('draw.dt', function() {
+    // set max width on dataTable
+    $(this).css('width', '100%');
+
     // add label for attribute for search
     $('.dataTables_filter label').attr('for', 'datatables-search');
     $('#DataTables_Table_0_filter').find('input[type="search"]').attr('id', 'datatables-search');
