b80437f08b2505fbcc75b0ae383873fcb555c791
Eric Mill <eric@konklone.com>
2016-08-21 19:44:26 -0400

    set the env var for the pshtt CLI on our production server

--- deploy/cron.sh
+++ deploy/cron.sh
@@ -3,6 +3,9 @@
 # Set the path to domain-scan.
 export DOMAIN_SCAN_PATH=/opt/scan/domain-scan/scan
 
+# Set the path to the pshtt CLI.
+export PSHTT_PATH=/opt/scan/pshtt/pshtt_cli
+
 # go to pulse environment home
 cd $HOME/pulse/$PULSE_ENV/current
 
