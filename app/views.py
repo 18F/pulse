610a7fc78ab2f4cdd5e847add718b5e55daee983
Eric Mill <eric@konklone.com>
2016-12-09 12:14:12 -0500

    Placeholder open data page (#597)
    * placeholder open data page
    
    * adding innards of the /data page
    
    * fixing formatting
    
    * adding in text
    
    * removing extra line breaks
    
    * adding some back in
    
    * fixing extra spacing in dap section
    
    * changing text

--- app/views.py
+++ app/views.py
@@ -7,6 +7,10 @@ import ujson
 
 def register(app):
 
+    @app.route("/data/")
+    def data():
+        return render_template("data.html")
+
     @app.route("/")
     def index():
         return render_template("index.html")
