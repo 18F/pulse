205e8f4be57068184a6a5585f0b2032d778f638e
Micah Saul <micah.saul@gsa.gov>
2017-07-24 11:45:34 -0700

    Remove extra parens
    And this is why I don't edit files in the browser.

--- app/models.py
+++ app/models.py
@@ -200,7 +200,7 @@ class Agency:
   # An agency which had at least 1 eligible domain.
   def eligible(report_name):
     return db.table('agencies').search(
-      (Query()[report_name]['eligible'] > 0)
+      Query()[report_name]['eligible'] > 0
     )
 
   # Create a new Agency record with a given name, slug, and total domain count.
