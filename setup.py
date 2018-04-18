4bb88ce614f380a03b7a662888cf286322fa0c9c
David Buckley <buckley.w.david@gmail.com>
2018-04-18 11:41:12 -0400

    Data package setup (#2)
    add package setup file

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name='pulse',
+    version='0.0.1',
+    long_description='',
+    author='GSA 18F, CDS-SNC',
+    author_email='pulse@cio.gov, cds-snc@tbs-sct.gc.ca',
+    url='https://github.com/cds-snc/pulse',
+    packages=[
+        'data',
+    ],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+    ],
+)
